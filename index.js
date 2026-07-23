198359d672e269ca56bd229f64b7a71cccb5838b
ShivangSaxena05 <shivangsaxena40@gmail.com>
2026-07-23 23:00:15 +0530

    swagger

--- index.js
+++ index.js
@@ -85,7 +85,12 @@ const swaggerOptions = {
     },
     servers: [
       {
+        url: "https://admin-backend-01pa.onrender.com",
+        description: "Production server",
+      },
+      {
         url: `http://localhost:${process.env.PORT || 5000}`,
+        description: "Local server",
       },
     ],
   },
